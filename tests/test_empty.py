87caaee394385efd76a3ba57fcf3b5ebcab12ce6
manoflearning <77jwk0724@gmail.com>
2025-09-12 21:52:06 +0900

    ci: add format check

--- tests/test_empty.py
+++ tests/test_empty.py
@@ -1,9 +1,10 @@
 import unittest
 
+
 class TestEmpty(unittest.TestCase):
-  def test_empty(self):
-    return True
+    def test_empty(self):
+        return True
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
